51cd9e4e55a21bb70f5d2acb48d3db9b9e087331
yifei Weng <johnewong95@gmail.com>
2022-10-16 17:35:41 +0800

    init

---
+++ deploy/00-deploy-mock.js
@@ -0,0 +1,32 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+
+const BASE_FEE = ethers.utils.parseEther("0.25");
+const GAS_PRICE_LINK = 1e9;
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...");
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        });
+
+        log("Mocks Deployed!");
+        log("----------------------------------------------------------");
+        log(
+            "You are deploying to a local network, you'll need a local network running to interact"
+        );
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        );
+        log("----------------------------------------------------------");
+    }
+};
+
+module.exports.tags = ["all", "mocks"];
